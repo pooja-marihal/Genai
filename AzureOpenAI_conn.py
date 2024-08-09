c5153d3519770efeeb3939fe7c69b1be90e35283
pooja-marihal <poojam1150219@gmail.com>
2024-08-09 14:24:06 +0530

    Update AzureOpenAI_conn.py

--- AzureOpenAI_conn.py
+++ AzureOpenAI_conn.py
@@ -5,8 +5,11 @@ import streamlit as st
  
 # Set environment variables
 print("okkkk")
-api_key = st.secrets["AZURE_OPENAI_API_KEY"]
-endpoint = st.secrets["AZURE_OPENAI_ENDPOINT"]
+# api_key = st.secrets["AZURE_OPENAI_API_KEY"]
+# endpoint = st.secrets["AZURE_OPENAI_ENDPOINT"]
+api_key = "de87392f71a84ccaa4e62ec942c56f6c"
+endpoint = "https://idea-4-azure-open-ai.openai.azure.com/"
+ 
 
 print("api_keyyyyyy",api_key,endpoint)
 # Initialize the AzureOpenAI client
