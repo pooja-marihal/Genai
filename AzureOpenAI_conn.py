39ce191f48fc41e49842321b4136d934172a31fb
pooja-marihal <poojam1150219@gmail.com>
2024-08-09 12:53:23 +0530

    Update AzureOpenAI_conn.py

--- AzureOpenAI_conn.py
+++ AzureOpenAI_conn.py
@@ -4,13 +4,15 @@ from openai import AzureOpenAI
 import streamlit as st
  
 # Set environment variables
+print("okkkk")
 api_key = st.secrets["AZURE_OPENAI_API_KEY"]
 endpoint = st.secrets["AZURE_OPENAI_ENDPOINT"]
 
- 
+print("api_keyyyyyy",api_key,endpoint)
 # Initialize the AzureOpenAI client
 client = AzureOpenAI(
     azure_endpoint=endpoint,
     api_key=api_key,  
     api_version="2024-05-13"
 )
+print("connectedddd",client)
